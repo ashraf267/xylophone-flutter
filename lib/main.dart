77d1f13cda28828c126db475ff16ae8cd2290b27
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-06-05 22:35:36 +0100

    task: DRY, status: Mission Accomplished

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,23 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
-  void playSound(int soundNumber) {
-    final player = AudioCache();
-    player.play('assets_note$soundNumber.wav');
+  // void playSound(int soundNumber) {
+  //   final player = AudioCache();
+  //   player.play('assets_note$soundNumber.wav');
+  // }
+
+  Widget buildKey(int soundNumber, Color? keyColor) {
+    return Expanded(
+      child: TxtBtn(
+        onTap: () {
+          final player = AudioCache();
+          player.play('assets_note$soundNumber.wav');
+        },
+        decoration: BoxDecoration(
+          color: keyColor,
+        ),
+      ),
+    );
   }
 
   @override
@@ -18,76 +32,13 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(1);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(2);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.orange,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(3);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.yellow,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(4);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(5);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.teal,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(6);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TxtBtn(
-                  onTap: () {
-                    playSound(7);
-                  },
-                  decoration: const BoxDecoration(
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
             ],
           ),
         ),
