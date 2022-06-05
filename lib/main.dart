468c074a9d5ba083901b36affde4559272a8f04c
Ashraf Otagun <otagunolaniyi7@gmail.com>
2022-06-05 10:27:41 +0100

    created a custom TextButton widget without a required child called TxtBtn

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,121 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('assets_note1.wav');
-              },
-              child: const Text('Click me'),
-            ),
+          child: Column(
+            children: [
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note1.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note2.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note3.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note4.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note5.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note6.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TxtBtn(
+                  onTap: () {
+                    final player = AudioCache();
+                    player.play('assets_note7.wav');
+                  },
+                  decoration: const BoxDecoration(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+// My custom TextButton called TxtBtn
+class TxtBtn extends StatelessWidget {
+  const TxtBtn(
+      {Key? key,
+      required this.onTap,
+      this.width = 250,
+      this.height = 50,
+      this.decoration,
+      this.child})
+      : super(key: key);
+
+  final void Function()? onTap;
+  final double width;
+  final double height;
+  final Decoration? decoration;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: decoration,
+        constraints: BoxConstraints.expand(
+          width: width,
+          height: height,
+        ),
+        child: child,
       ),
     );
   }
